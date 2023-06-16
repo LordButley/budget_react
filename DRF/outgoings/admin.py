7775cbe6488adc1535cd5f1dce5bc79b79d28d4d
Joshy Wilson <joshjwwilson@gmail.com>
2023-06-16 15:27:43 +0100

    add admin.py

--- DRF/outgoings/admin.py
+++ DRF/outgoings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import OutgoingsCategory, Payment, RecurringPayment
+
 # Register your models here.
+
+admin.site.register(OutgoingsCategory)
+admin.site.register(RecurringPayment)
+admin.site.register(Payment)

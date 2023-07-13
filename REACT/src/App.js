f47fa7da16c4898655da53106b287bfb0aaca3d9
Joshy Wilson <joshjwwilson@gmail.com>
2023-07-13 12:06:49 +0100

    add FA icons to sidebar

--- REACT/src/App.js
+++ REACT/src/App.js
@@ -2,10 +2,8 @@ import './App.css';
 import NavBar from './components/NavBar';
 
 import {
-  BrowserRouter,
   Routes,
   Route,
-  Link,
 } from "react-router-dom";
 import SideBar from './components/SideBar';
 import Container from 'react-bootstrap/Container';
@@ -20,6 +18,26 @@ function App() {
         <Row className = "min-vh-100">
           <Col sm="2">
             <SideBar/>
+          </Col>
+          <Col>
+            <Routes>
+              <Route>
+                exact path="/"
+                element = {}
+              </Route>
+              <Route>
+                exact path="/combined"
+                element = {}
+              </Route>
+              <Route>
+                exact path="/income"
+                element = {}
+              </Route>
+              <Route>
+                exact path="/outgoing"
+                element = {}
+              </Route>
+            </Routes>
           </Col>
         </Row>
       </Container>
--- REACT/src/components/SideBar.js
+++ REACT/src/components/SideBar.js
@@ -2,32 +2,47 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import { Link } from 'react-router-dom';
 import appStyles from "../styles/SideBar.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse, faPeopleArrows, faCartShopping, faSterlingSign } from '@fortawesome/free-solid-svg-icons'
+
 
 const SideBar = () => {
-  return (
-    <Container className="d-flex flex-column justify-content-center min-vh-100 m-auto ">
-        <div>
-        <Link className={appStyles.sidebarlinks}>
-            Dashboard
-        </Link>
-        </div>
-        <div>
-        <Link>
-            Combined
-        </Link>
-        </div>
-        <div>
-        <Link>
-            Income
-        </Link>
-        </div>
-        <div>
-        <Link>
-            Outgoings
-        </Link>
-        </div>
+    return (
+        <Container className={`d-flex flex-column m-auto mt-5 ${appStyles.sidebarcontainer}`}>
+            <div className={appStyles.sidebardivs}>
+                <Link className={appStyles.sidebarlinks}>
+                    <FontAwesomeIcon className={appStyles.sidebaricons} icon={faHouse} />
+                    <span>
+                        Dashboard
+                    </span>             
+                    </Link>
+            </div>
+            <div className={appStyles.sidebardivs}>
+                <Link className={appStyles.sidebarlinks}>
+                    <FontAwesomeIcon className={appStyles.sidebaricons} icon={faPeopleArrows} />
+                    <span>
+                        Combined
+                    </span>            
+                    </Link>
+            </div>
+            <div className={appStyles.sidebardivs}>
+                <Link className={appStyles.sidebarlinks}>
+                    <FontAwesomeIcon className={appStyles.sidebaricons} icon={faSterlingSign} />
+                    <span>
+                        Income
+                    </span>
+                    </Link>
+            </div>
+            <div className={appStyles.sidebardivs}>
+                <Link className={appStyles.sidebarlinks}>
+                    <FontAwesomeIcon className={appStyles.sidebaricons} icon={faCartShopping} />
+                    <span>
+                        Outgoings
+                    </span>
+                </Link>
+            </div>
 
-    </Container>
+        </Container>
     )
 }
 
